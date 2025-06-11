200fc7b980f41109fad5025b6a44f5388f42816b
Spiceses <1827243525@qq.com>
2025-06-11 21:59:17 +0800

    更新blog page

--- client/src/pages/BlogPage.tsx
+++ client/src/pages/BlogPage.tsx
@@ -1,7 +1,22 @@
+import { Typography } from "@mui/material";
 import Blog from "../components/Blog/Blog.tsx";
 
 const BlogPage = () => {
-  return <Blog />;
+  return (
+    <>
+      <Typography
+        variant="h3"
+        align="center" // 水平居中
+        sx={{
+          marginTop: 4, // 上边距，数字代表 theme.spacing() 的倍数，例如 4 * 8px = 32px
+          marginBottom: 4, // 下边距
+        }}
+      >
+        博客
+      </Typography>
+      <Blog />
+    </>
+  );
 };
 
 export default BlogPage;
