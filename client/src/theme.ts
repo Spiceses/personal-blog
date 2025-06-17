d16b0ecd20353d2c983577e5adfe63b868bb5462
Spiceses <1827243525@qq.com>
2025-06-18 01:42:57 +0800

    配置网站字体

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,6 +1,15 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
+  palette: {
+    mode: "dark", // 关键！将模式设置为 'dark'
+  },
+  typography: {
+    fontFamily: [
+      // 首先列出你想要的Serif中文字体
+      "Noto Serif SC", // 如果你使用了Google Fonts的Noto Serif SC
+    ].join(","),
+  },
   components: {
     MuiCssBaseline: {
       styleOverrides: {
