44cac4753a327f470611f5578f74453131df60ba
Spiceses <1827243525@qq.com>
2025-09-10 14:31:53 +0800

    完善 AboutPage 内容

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,4 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
 const AboutPage = () => {
-  return <>about page</>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Typography variant="h5">Hi, 我是Spices! 如你所见, 这是我的个人博客网站, 我将在这里向你讲述我的故事!</Typography>
+    </Box>
+  );
 };
 export default AboutPage;
