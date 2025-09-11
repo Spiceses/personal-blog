30ff9012e771784b5ab7be4e66891b566fec9c22
Spiceses <1827243525@qq.com>
2025-09-11 14:17:18 +0800

    在导航栏添加用户 avatar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-import { AppBar, Avatar, Box, Button, Toolbar } from "@mui/material";
-import { Link } from "react-router-dom";
+import { AppBar, Avatar, Box, Button, Toolbar, IconButton } from "@mui/material";
+import { Link } from "react-router-dom"; // 只需 Link
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import avatar from "../../assets/avatar.jpg";
 
 const Navbar = () => {
@@ -7,9 +8,8 @@ const Navbar = () => {
     <AppBar position="static" color="inherit">
       <Toolbar>
         <Avatar alt="my avatar" src={avatar} />
-        <Box
-          sx={{ display: "flex", flexGrow: 1, justifyContent: "center" }}
-        ></Box>
+        <Box sx={{ flexGrow: 1 }} />
+
         <Button color="inherit" component={Link} to="/">
           主页
         </Button>
@@ -19,6 +19,17 @@ const Navbar = () => {
         <Button color="inherit" component={Link} to="/about">
           关于
         </Button>
+
+        {/* 直接链接到用户主页，无需任何点击事件处理 */}
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          component={Link} // 使用 Link 组件
+          to="/profile" // 直接指向目标地址
+        >
+          <AccountCircle />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
