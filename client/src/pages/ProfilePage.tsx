3750281a580621d77336c50c58dece8493774365
Spiceses <1827243525@qq.com>
2025-09-11 14:16:18 +0800

    实现 profile 页面

---
+++ client/src/pages/ProfilePage.tsx
@@ -0,0 +1,10 @@
+const ProfilePage = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>用户主页</h1>
+      <p>欢迎！只有已登录的用户才能看到此页面。</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
